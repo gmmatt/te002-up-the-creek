9e076d6b8d3fd0c70f3c1a4cb1736314708980fb
Gustl80 <hgrillenberger@gmx.at>
2023-07-15 22:10:07 +0200

    Update V11

---
+++ te002-up-the-creek.js
@@ -0,0 +1,44 @@
+import { registerUpTheCreekSettings } from "./settings.js";
+
+Hooks.once("init", async function() {
+    console.log("torgeternity | Initializing Hostel Takeover Adventure Module");
+
+    //-----system settings
+    registerUpTheCreekSettings()
+
+})
+Hooks.on("ready", async function() {
+
+
+    //----rendering welcome message
+
+    let welcomeMessage = await renderTemplate("modules/te002-up-the-creek/welcomeMessage.hbs");
+
+    if (game.settings.get("te002-up-the-creek", "welcomeMessage") == true) {
+        let d = new Dialog({
+            title: "Welcome to the Up the Creek Introductory Adventure Module!",
+            content: welcomeMessage,
+            buttons: {
+                one: {
+                    icon: '<i class="fas fa-check"></i>',
+                    label: "OK",
+                },
+                two: {
+                    icon: '<i class="fas fa-ban"></i>',
+                    label: "Don't show again",
+                    callback: () =>
+                        game.settings.set("te002-up-the-creek", "welcomeMessage", false),
+                },
+            },
+        }, {
+            left: 100,
+            top: 100,
+            height: 425,
+            width: 450,
+            resizable: false
+        });
+        d.render(true);
+    }
+
+
+})
